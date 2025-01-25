5ee4c9b87135d3d5ebcce9bff03b8f433d4cdcad
MariiaKpln <kpln.mary@gmail.com>
2025-01-25 20:54:29 +0200

    animator js

--- src/animator.js
+++ src/animator.js
@@ -1,5 +1,4 @@
-let boxAnimationElement = document.getElementById("#box_animate");
-
+let boxAnimationElement = document.getElementById("box_animate");
 function startStop() {
   boxAnimationElement.classList.toggle("animation-pop");
 }
