1a43581546baa1c098b823d22f1c9267d9f18152
MariiaKpln <kpln.mary@gmail.com>
2025-01-26 09:32:32 +0200

    Homework

--- src/animator.js
+++ src/animator.js
@@ -1,4 +1,10 @@
-let boxAnimationElement = document.getElementById("box_animate");
-function startStop() {
-  boxAnimationElement.classList.toggle("animation-pop");
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+
+function start() {
+  box2.classList.add("animate");
+}
+
+function stop() {
+  box2.classList.remove("animate");
 }
